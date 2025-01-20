691581e2acd2b208a4ba8237dcc703642ee916ab
dyaccu <dyaccu@gmail.com>
2025-01-19 21:35:33 -0300

    crea funcion agregraNombre

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeNombres = [];
+
+function agregarNombre(){
+    let nombres = document.getElementById('amigo').value;
+    if (nombres === "") {
+        alert('Por favor, ingrese un nombre')
+    } else {
+        listaDeNombres.push(nombres);
+    } limpiarCampo();
+}
+
+function limpiarCampo(){
+    document.querySelector('#amigo').value='';
+}
